0393cc23cf0b0100cb5476c198ca16ffc465e831
Zack <zbristor@gmail.com>
2017-07-12 20:12:52 -0400

    new navbar in place

--- src/main/java/zack/configs/SecurityConfiguration.java
+++ src/main/java/zack/configs/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register","/upload", "memelink/**").permitAll()
+                .antMatchers("/", "/register","/upload", "memelink/**","/myfeed","/mypics","/searchusers").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
